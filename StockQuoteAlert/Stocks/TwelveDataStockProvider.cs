4c996d99f86d5bf375ff94d526ba72716a056ad5
Thomas <mergener.thomas@gmail.com>
2025-04-05 21:50:53 -0300

    Added Twelve Data Stock Provider

---
+++ StockQuoteAlert/Stocks/TwelveDataStockProvider.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+using static System.Net.WebRequestMethods;
+
+namespace StockQuoteAlert.Stocks
+{
+    internal class TwelveDataStockProvider : IStockProvider, IDisposable
+    {
+        #region Fields
+        private readonly HttpClient httpClient = new();
+        private readonly string apiKey;
+        private bool disposedValue;
+        #endregion
+
+        private record class GetPriceResponse([property: JsonPropertyName("price")] string Price);
+
+        public async Task<decimal?> GetLatestStockPrice(string stockName)
+        {
+            var resp = await httpClient.GetFromJsonAsync<GetPriceResponse>($"/price?apikey={apiKey}&symbol={stockName}");
+
+            // Make sure we parse with "InvariantCulture", otherwise we may get
+            // wrong decimal points.
+            if (decimal.TryParse(resp?.Price,
+                                 NumberStyles.Number,
+                                 CultureInfo.InvariantCulture, 
+                                 out var price))
+            {
+                return price;
+            }
+            return null;
+        }
+
+        #region Constructor
+        public TwelveDataStockProvider(string apiKey, string baseUrl = "https://api.twelvedata.com/")
+        {
+            this.apiKey = apiKey;
+            httpClient.BaseAddress = new Uri(baseUrl);
+        }
+        #endregion
+
+        #region Disposable
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    httpClient.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
